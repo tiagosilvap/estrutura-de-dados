6a97119ae27461f5de5437fdc8df13f3fe5f589a
Tiago Pereira <tiago.pereira@hotmart.com>
2023-11-26 17:02:23 -0300

    first commit

---
+++ src/ordenacaoepesquisa/pesquisa/PesquisaSequencial.java
@@ -0,0 +1,12 @@
+package ordenacaoepesquisa.pesquisa;
+
+public class PesquisaSequencial {
+
+	public static int pesquisaSequencial(int[] vetor, int n) {
+		for (int i = 0; i < vetor.length; i++) {
+			if (vetor[i] == n) 
+				return i;
+		}
+		return -1;
+	}
+}
